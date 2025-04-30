6e425f116161e78f9a400ec966f9a8339341ee04
Teerapat6509650468 <teerapat.kir@dome.tu.ac.th>
2025-04-30 23:50:04 +0700

    Init laptop-store

---
+++ laptop-store/src/test/java/cs/tu/laptop_store/LaptopStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.tu.laptop_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
