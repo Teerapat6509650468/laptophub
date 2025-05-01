cddbc86914a5335df7b73b43780b394bc3f37fd0
Teerapat6509650468 <teerapat.kir@dome.tu.ac.th>
2025-05-02 03:09:04 +0700

    more context on laptop side
    When click Reserve Laptop in Laptop Store it will allow Laptop Customer to perform reserve action.

--- laptop-store/src/main/java/cs/tu/laptop_store/LaptopController.java
+++ laptop-store/src/main/java/cs/tu/laptop_store/LaptopController.java
@@ -15,6 +15,7 @@ public class LaptopController {
 
     private final LaptopService laptopService;
     private boolean laptopsDisplayEnabled = false;
+    private boolean reservationEnabled = false;
 
     @Autowired
     public LaptopController(LaptopService laptopService) {
@@ -35,12 +36,25 @@ public class LaptopController {
         return ResponseEntity.ok("Laptop display enabled");
     }
 
+    @PostMapping("/enable-reservation")
+    public ResponseEntity<String> enableReservation() {
+        reservationEnabled = true;
+        return ResponseEntity.ok("Laptop reservation enabled");
+    }
+
+    @GetMapping("/reservation-status")
+    public ResponseEntity<Boolean> getReservationStatus() {
+        return ResponseEntity.ok(reservationEnabled);
+    }
+
     @PostMapping("/{id}/reserve")
     public ResponseEntity<String> reserveLaptop(@PathVariable Long id) {
+        if (!reservationEnabled) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Reservations are not enabled yet");
+        }
         boolean success = laptopService.reserveLaptop(id);
         if (success) {
-            return ResponseEntity.status(HttpStatus.OK).body("Laptop reserved successfully!.");
-            
+            return ResponseEntity.ok("Laptop reserved successfully!");
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Laptop is already reserved or not found.");
         }
