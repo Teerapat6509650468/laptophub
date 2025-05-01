e6e272a80f4b218c309609d0c10753455c3ea835
Teerapat6509650468 <teerapat.kir@dome.tu.ac.th>
2025-05-01 23:51:55 +0700

    modify frontend for better context

--- laptop-store/src/main/java/cs/tu/laptop_store/LaptopStoreApplication.java
+++ laptop-store/src/main/java/cs/tu/laptop_store/LaptopStoreApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LaptopStoreApplication {
@@ -17,4 +19,16 @@ public class LaptopStoreApplication {
         return new RestTemplate();
     }
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8081")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
